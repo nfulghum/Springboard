49ae82bff6d1a4ba0c1c4267295978dbe6c65054
nfulghum <103339586+nfulghum@users.noreply.github.com>
2022-04-11 15:19:17 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-function countDown(num) {
-    let timer = setInterval(function () {
-        num--;
-        if (num <= 0) {
-            clearInterval(num);
-            console.log('Done!')
-        }
-        else {
-            console.log(num)
-        }
-    }, 1000)
-}
+// // 1.
+// document.getElementById('container');
 
-function randomGame() {
-    let num;
-    let timesPicked = 0;
-    let timer = setInterval(function () {
-        num = Math.random();
-        timesPicked++;
-        if (num > .75) {
-            clearInterval(timer);
-            console.log(timesPicked + ' attempts')
-        }
-    }, 1000)
-}
+// // 2.
+// document.querySelector('#container');
+
+// // 3.
+// document.getElementsByClassName('second');
+
+// // 4.
+// document.getElementsByClassName('third')[1];
+
+// // 5.
+// const newText = document.querySelector('#container');
+// newText.innerText = 'Hello'
+
+// // 6.
+// const footer = document.querySelector('.footer');
+// footer.classList.add('main')
+
+// 7.
+// footer.classList.remove('main')
+
+// 8.
+// const newList = document.createElement('li');
+
+// 9.
+// newList.textContent = 'four';
+
+// 10.
+// const ul = document.querySelector('ul');
+// ul.append(newList)
+
+// 11.
+// const allList = document.querySelectorAll('ol li');
+
+// for (let li of allList) {
+//     li.style.backgroundColor = 'green';
+// }
+
+// 12.
+// const footer = document.querySelector('.footer');
+// footer.remove()
